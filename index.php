da36aa1d9a6020456ed0d86e0f4d3cb68a871d88
xbaee <aldoweah21@gmail.com>
2022-08-01 02:31:08 +0530

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet repellendus assumenda maxime vel fugit sint quo soluta sequi quae, voluptates optio quibusdam nemo quia autem libero asperiores modi enim necessitatibus?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
